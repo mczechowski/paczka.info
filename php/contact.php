51b6980dcf0750013182eb68073d4037a27c5f76
Mariusz <m.czechowski@interia.pl>
2017-11-12 16:01:05 +0100

    Add contact.php

---
+++ php/contact.php
@@ -0,0 +1,42 @@
+<?php
+// Zmienna dla Imienia i nazwiska
+$name = $_POST['ImieNazwisko'];
+
+// Zmienna dla adresu email
+$email = $_POST['email'];
+
+// Zmienna dla treści wiadomości
+$message = $_POST['message'];
+
+// Email z którego ma być wysłana wiadomość
+$sourceMail = "kontakt@paczka.info";
+
+// Email na który ma zostać wysłana wiadomość
+$destynationMail = "biuro@paczka.info";
+
+// Tytuł wiadomości
+$title = "Wiadomość z formularza na stronie paczka.info";
+
+// Treść wiadomości
+$wiadomosc = "";
+$wiadomosc .= "Imie i nazwisko: " . $name . "\n";
+$wiadomosc .= "Email: " . $email . "\n";
+$wiadomosc .= "Wiadomość: " . $message . "\n";
+
+// Dodanie kodowania UTF-8 do nagłowka wiadomości
+$header ="";
+$header .= "Od: " . $sourceMail . "\n";
+$header .= "Content-Type:text/plain;charset=utf-8";
+
+// Wysłanie wiadomości
+$success = mail($destynationMail, $title, $message, $header);
+
+// Przekierowywujemy na potwierdzenie
+if ($succces){
+        print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.php\">";
+    }
+else{
+    print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+    }
+    
+?>
